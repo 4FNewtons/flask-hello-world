a168957392b943bb21da2342dcb9a8bf0ca40b15
4FNewtons <141512269+4FNewtons@users.noreply.github.com>
2023-08-06 14:25:33 +0600

    Update main.py

--- main.py
+++ main.py
@@ -435,8 +435,8 @@ def index():
     print('Невозможно связаться с БД')
 
   return render_template('index.html',
-                        #  username=username,
-                        #  posts=postlist_str,
+                         username=username,
+                         posts=postlist_str,
                          list_of_projects=postsdesc)
                         #  message=message)
 
